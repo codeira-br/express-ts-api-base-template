2b2212db4f7a137c839dd6436dc65fc7b2404223
Deft <yp4iafp1d@mozmail.com>
2026-09-11 14:33:13 -0300

    feat(shared): create logger

---
+++ src/shared/lib/logger.ts
@@ -0,0 +1,21 @@
+import pino from 'pino'
+
+import env from '../config/env.js'
+
+const logger = pino({
+  level: env.LOG_LEVEL,
+
+  transport:
+    env.NODE_ENV !== 'production'
+      ? {
+          target: 'pino-pretty',
+          options: {
+            colorize: true,
+            translateTime: 'SYS:standard',
+            ignore: 'pid,hostname',
+          },
+        }
+      : undefined,
+})
+
+export default logger
